41bd3752a1df5a0438eeb93932aeb8c057d0e4a5
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-06-13 13:52:23 -0300

    Update CardList.tsx

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,26 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const [urlImage, setUrlImage] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
-
+  function handleViewImage(url: string): void {
+    setUrlImage(url);
+    onOpen();
+  }
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={10}>
+        {cards.map(card => (
+          <Card
+            key={card.id}
+            viewImage={() => handleViewImage(card.url)}
+            data={card}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={urlImage} />
     </>
   );
 }
